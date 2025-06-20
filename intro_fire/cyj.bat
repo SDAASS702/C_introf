4c95bdf157347a308d6820ac998203c8244f482e
도윤 <doyoon@example.com>
2025-06-20 11:34:56 +0900

    내부 Git 제거 후 전체 커밋

---
+++ intro_fire/cyj.bat
@@ -0,0 +1,8 @@
+git config --global user.name "choiyeji"
+git config --global user.email "choiyeji@gmail.com"
+
+cd C:\Users\User\Documents
+
+mkdir c_lesson
+
+git clone https://github.com/chldpwl03/c_lesson.git
